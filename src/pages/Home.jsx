6a92c51577a93cf53b98b25b98218bebe2ccdf03
vidhi agarwal <vidhi.agarwal2018@vitstudent.ac.in>
2021-03-20 15:45:39 +0530

    Added Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,38 @@
 import { Button, Grid, makeStyles } from "@material-ui/core";
-import React from "react";
+import React, { Component }from "react";
+//import { ReactComponent as wave } from '../assets/wave.svg';
+import wave from '../assets/wave.svg'
+import '../Home.css'
+import logo from '../assets/logo.png'
+import { NavLink } from 'react-router-dom'
+
+
 
 function Home() {
 
-    return (<>
-        Hi
-    </>);
+    return (
+        <>
+        <div className="nav">
+            
+            <img src={logo} />
+            <div className="options">
+            <NavLink className='navBarLink' to="/">About</NavLink>
+            <NavLink className='navBarLink' to="/product">Services</NavLink>
+            <NavLink className='navBarLink' to="/product">Contact Us</NavLink>
+            <Button variant="outlined" color="primary" href="#"> Sign In </Button>
+            <Button variant="contained" color="primary" href="#" > Sign Up </Button>
+            </div>
+        </div>
+    <div className="wave">
+    
+        <img src={wave} />
+        
+    </div>
+    <div className="main">
+        <h1>Medicist</h1>
+        <h2>Not Your Regular Assistant</h2>
+    </div>
+    </>
+      );
 }
 export default Home;
