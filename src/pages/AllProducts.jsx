7145d65ad544f7efdd97b56ecf057be3bd01dcc9
Adictya <aditya.26101@gmail.com>
2021-03-21 00:17:23 +0530

    added product images

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -3,16 +3,20 @@ import React, {Component} from "react";
 import classes from "../nav.module.css";
 import navim1 from '../assets/navim1.svg'
 import navim2 from '../assets/navim2.svg'
+import ProductCategories from '../components/productCategories'
 
 
 function allProducts() {
     return (
-        <>
-        <div className={classes.nav}>
-        <img src={navim1} alt="prod" className="navprod" />
-        <img src={navim2} alt="assis" className="navassis" />
+        <div className={classes.mainContainer}>
+            <div className={classes.nav}>
+                <img src={navim1} alt="prod" className="navprod" />
+                <img src={navim2} alt="assis" className="navassis" />
+            </div>
+            <div className={classes.content}>
+                <ProductCategories/>
+            </div>
         </div>
-        </>
 
     );
 }
