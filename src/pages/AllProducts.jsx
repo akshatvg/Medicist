39584d1f09107634eb5b976a00e4aa4e07fe387c
Adictya <aditya.26101@gmail.com>
2021-03-21 01:30:54 +0530

    added header

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -1,12 +1,15 @@
 
-import React, {Component} from "react";
+import React, {Component, useState} from "react";
 import classes from "../nav.module.css";
 import navim1 from '../assets/navim1.svg'
 import navim2 from '../assets/navim2.svg'
+import cartIcon from '../assets/bx_bx-cart.svg'
 import ProductCategories from '../components/productCategories'
 
 
-function allProducts() {
+function AllProducts() {
+    const [page,setPage] = useState(0)
+    const [rent,setRent] = useState(true)
     return (
         <div className={classes.mainContainer}>
             <div className={classes.nav}>
@@ -14,9 +17,14 @@ function allProducts() {
                 <img src={navim2} alt="assis" className={classes.navassis} />
             </div>
             <div className={classes.content}>
+                <div className={classes.header}>
+                    <div className={classes.heading}> Rent Supplies </div>
+                    <div className={classes.subHeading}> / Pulse Oximeter </div>
+                    <button className = {classes.cartButton}> <img className={classes.cartIcon} src={cartIcon}/> Cart </button>
+                </div>
                 <ProductCategories />
             </div>
         </div>
     );
 }
-export default allProducts;
+export default AllProducts;
