be8a927cbaa300ba78e27eca4cc9c4d00134d954
vidhi agarwal <vidhi.agarwal2018@vitstudent.ac.in>
2021-03-20 22:10:39 +0530

    login page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,125 @@
-import { Button, Grid, makeStyles } from "@material-ui/core";
-import React, { Component }from "react";
+import {Button, Grid, makeStyles} from "@material-ui/core";
+import React, {Component, useState} from "react";
 //import { ReactComponent as wave } from '../assets/wave.svg';
-import wave from '../assets/wave.svg'
-import '../Home.css'
+
 import logo from '../assets/logo.png'
-import { NavLink } from 'react-router-dom'
+import classes from "../home.module.css";
+import formStyle from "../userForm.module.css";
+// import {NavLink} from 'react-router-dom'
+
 
 
 
 function Home() {
 
+    const [option, setOption] = useState(true)
+
+
+    function SignLinkHandler() {
+        if (option) {
+            setOption(false);
+        }
+    };
+
+    function LoginLinkHandler() {
+        if (!option) {
+            setOption(true);
+        }
+    };
+
+
     return (
         <>
-        <div className="nav">
-            
-            <img src={logo} />
-            <div className="options">
-            <NavLink className='navBarLink' to="/">About</NavLink>
-            <NavLink className='navBarLink' to="/product">Services</NavLink>
-            <NavLink className='navBarLink' to="/product">Contact Us</NavLink>
-            <Button variant="outlined" color="primary" href="#"> Sign In </Button>
-            <Button variant="contained" color="primary" href="#" > Sign Up </Button>
-            </div>
+            <div className={classes.LoginPane}>
+                <div className={classes.LogoBanner}>
+                    <div className={classes.LogoContainer}>
+                        <img src={logo} alt="Fforg" className={classes.Logo} />
+                    </div>
+                </div>
+                <div className="">
+                    <div className={classes.Itemlist}>
+                        <div
+                            className={
+                                option
+                                    ? classes.active
+                                    : classes.LoginLink
+                            }
+                            onClick={LoginLinkHandler}
+                        >
+                            Login
         </div>
-    <div className="wave">
-    
-        <img src={wave} />
-        
-    </div>
-    <div className="main">
-        <h1>Medicist</h1>
-        <h2>Not Your Regular Assistant</h2>
-    </div>
-    </>
-      );
+                        <div
+                            className={
+                                option
+                                    ? classes.SignLink
+                                    : classes.activeMargin
+                            }
+                            onClick={SignLinkHandler}
+                        >
+                            SignUp
+        </div>
+                        <div className={classes.Underbar}></div>
+                    </div>
+                </div>
+                <div className={formStyle.UserForm}>
+                    {option ?
+                        (
+                            <>
+                                <input
+                                    type="text"
+                                    placeholder="Email"
+                                    className={formStyle.InputField}
+                                    label="Username"
+                                />
+                                <input
+                                    type="password"
+                                    placeholder="enter password"
+                                    className={formStyle.InputField}
+                                    style={{marginTop: "25px"}}
+                                    label="Password"
+                                />
+                            </>
+                        ) : (
+
+                            <>
+                                <input
+                                    type="text"
+                                    placeholder="Email"
+                                    className={formStyle.InputField}
+                                    label="Username"
+                                />
+                                <input
+                                    type="password"
+                                    placeholder="enter password"
+                                    className={formStyle.InputField}
+                                    style={{marginTop: "25px"}}
+                                    label="Password"
+                                />
+                            </>
+                        )
+                    }
+                </div>
+                <p className={classes.message} id="msg"></p>
+                <div className={formStyle.UserButtons}>
+                    <button
+                        type="submit"
+                        className={formStyle.SubButton}
+                    // onClick={SubmitHandler}
+                    >
+                        Submit
+        </button>
+                </div>
+                <div style={{marginTop: "105px"}}>
+                </div>
+                <div className={classes.DSCLogoContainer}>
+                    <a href="https://dscvit.com">
+                    </a>
+                </div>
+            </div>
+        <div >
+            
+        </div>
+        </>
+    );
 }
 export default Home;
