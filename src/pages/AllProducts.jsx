fb185895bd75dd2ab9d2041e8a369d1b1c9f413e
Adictya <aditya.26101@gmail.com>
2021-03-21 00:22:55 +0530

    fixed conflicts

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -10,18 +10,13 @@ function allProducts() {
     return (
         <div className={classes.mainContainer}>
             <div className={classes.nav}>
-                <img src={navim1} alt="prod" className="navprod" />
-                <img src={navim2} alt="assis" className="navassis" />
+                <img src={navim1} alt="prod" className={classes.navprod} />
+                <img src={navim2} alt="assis" className={classes.navassis} />
             </div>
             <div className={classes.content}>
-                <ProductCategories/>
+                <ProductCategories />
             </div>
-        <>
-        <div className={classes.nav}>
-        <img src={navim1} alt="prod" className={classes.navprod} />
-        <img src={navim2} alt="assis" className={classes.navassis} />
         </div>
-
     );
 }
 export default allProducts;
