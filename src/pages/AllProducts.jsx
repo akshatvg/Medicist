82bbf218686891924f388c723a9924e09aac1bbc
vidhi agarwal <vidhi.agarwal2018@vitstudent.ac.in>
2021-03-21 00:16:55 +0530

    navbar

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -9,8 +9,8 @@ function allProducts() {
     return (
         <>
         <div className={classes.nav}>
-        <img src={navim1} alt="prod" className="navprod" />
-        <img src={navim2} alt="assis" className="navassis" />
+        <img src={navim1} alt="prod" className={classes.navprod} />
+        <img src={navim2} alt="assis" className={classes.navassis} />
         </div>
         </>
 
